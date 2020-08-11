a6fbff854bc341f5f61d9ed8b12cb3ba39cf2a15
Edin Dudojević <edudojevic@users.noreply.github.com>
2020-08-11 14:42:42 +0200

    Added solution for event-driven model exercise

---
+++ exercises/2-event-driven-model/javascript/solution/services/orders/server.js
@@ -0,0 +1,48 @@
+const amqp = require('amqplib')
+const express = require('express')
+const app = express()
+app.use(express.json())
+const port = 3001
+
+const defaultExchange = 'amq.default'
+const amqpUrl = 'amqp://localhost'
+
+async function publishToChannel(channel, { routingKey, exchange, data }) {
+    return new Promise((resolve, reject) => {
+        const dataBuffer = Buffer.from(JSON.stringify(data), 'utf-8');
+
+        channel.publish(exchange, routingKey, dataBuffer, { persistence: true }, err => {
+            if (err) {
+                return reject(err)
+            }
+            resolve()
+        })
+    })
+}
+
+async function connectToRabbitMq() {
+    let connection = await amqp.connect(amqpUrl);
+    return connection.createConfirmChannel();
+}
+
+const controller = {
+    placeOrder: async function(req, res) {
+        let channel = await connectToRabbitMq()
+
+        // publish the data to Rabbit MQ
+        let { customerId, products } = req.body;
+        console.log('publishing data to channel')
+        await publishToChannel(channel, { routingKey: "order_placed", exchangeName: defaultExchange, data: { customerId, products } });
+        res.send('ok')
+    }
+}
+
+const routes = function(app) {
+    app.route('/orders')
+       .post(controller.placeOrder)
+}
+routes(app)
+
+app.listen(port, function() {
+   console.log('Server started on port: ' + port)
+})
